91c0ce51d0bba1886268109433267b269f5c52a6
leesimond <simondlee@gmail.com>
2016-09-25 18:10:09 +1000

    Fixed path

--- static/js/bestmenu-ajax.js
+++ static/js/bestmenu-ajax.js
@@ -1,7 +1,7 @@
 $('#likes').click(function(){
 	var catid;
 	catid = $(this).attr("data-catid");
-	$.get('/like_category/', {category_id: catid}, function(data){
+	$.get('like_category/', {category_id: catid}, function(data){
 		$('#like_count').html(data);
 		$('#likes').hide();
 	});
@@ -10,7 +10,7 @@ $('#likes').click(function(){
 $('#suggestion').keyup(function(){
 	var query;
 	query = $(this).val();
-	$.get('/suggest_category/', {suggestion: query}, function(data){
+	$.get('suggest_category/', {suggestion: query}, function(data){
 		$('#cats').html(data);
 	});
 });
@@ -20,7 +20,7 @@ $('.restaurant-add').click(function(){
 	var url = $(this).attr("data-url");
 	var title = $(this).attr("data-title");
 	var me = $(this)
-	$.get('/auto_add_restaurant/', {category_id: catid, url:url, title: title}, function(data){
+	$.get('auto_add_restaurant/', {category_id: catid, url:url, title: title}, function(data){
 		$('#restaurants').html(data);
 		me.hide();
 	});
